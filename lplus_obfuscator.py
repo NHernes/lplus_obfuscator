f1ff4622d661fab2dbdf61cad49c999bfee2cdf7
Nils Hernes <100680594+NHernes@users.noreply.github.com>
2023-01-05 14:25:19 +0100

    Erweiterung um Salt

--- lplus_obfuscator.py
+++ lplus_obfuscator.py
@@ -1,97 +1,83 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-import base64
-import zlib
 import hashlib
 
-#https://nitratine.net/blog/post/asymmetric-encryption-and-decryption-in-python/
+
+########################
+#DESCRIPTION
+#This module obfuscates all predefined personal data of LPLUS REST responses and returns an anonymized response
+#The obfuscation method deployes SHA256 algorithm. This one-way encryption ensures irreversable obfuscation of personal data.
+
+#LICENSE
+#MIT License
+
+#Copyright (c) 2022 Nils Hernes
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+######################
+
+
+def traversing_nested_lists(json_response,data_to_obfuscate):
+    nested_dict=json_response
+
+    if isinstance(nested_dict,dict):
+        for key,item in nested_dict.items():
+            if isinstance(item,list) and item==True:
+                traversing_nested_lists(item[0],data_to_obfuscate) #When the item to the key is a nested dictionary itself, the function calls itself to access the next layer
+            else:
+                if key in data_to_obfuscate: #check if dictionary key is part of data to obfuscate
+                    item_encrypted=encryption(item) #when true: item to the key gets encrypted
+                    nested_dict[key]=item_encrypted #new item to key gets written in the response
 
 def encryption(item):
-    """
-    with open("public_key.pem", "rb") as key_file:
-        public_key = serialization.load_pem_public_key(
-            key_file.read(),
-            backend=default_backend()
-        )
-    """
-    print(item)
+    item=item
+    salt = "3ELT@k&Devc!4yxAg!X2"   
 
-    if isinstance(item,int):
-        item=str(item)
+    if isinstance(item,type(None)) or item=="None" or item=="": #Check if item to key contains personal data
+        return item #if not, the item is returned unencrypted
 
-    if isinstance(item,type(None)) or item=="None" or item=="":
-        return item
-    elif isinstance(item,str):
-        item_bytes=bytes(item, 'utf-8')
-
-    """
-    item_to_encrypt = item_bytes
+    elif isinstance(item,str): #check if item is text string
+        item_salted=item+salt
+        item_bytes=bytes(item_salted, 'utf-8')  #string is turned into bytes
     
-    
-    item_encrypted = public_key.encrypt(
-        item_to_encrypt,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    """
-    item_encrypted = hashlib.sha256(item_bytes).hexdigest()
+    elif isinstance(item,int): #check if item is integrer
+        item=str(item) #To turn data into hashable bytes, integers are turned into text
+        item_salted=item+salt
+        item_bytes=bytes(item_salted,'utf-8') #string is turned into bytes
 
-    return item_encrypted
+    item_encrypted = hashlib.sha256(item_bytes).hexdigest() #item is encrypted with SHA256 algorithm
+
+    return item_encrypted #encrypted item is returned
 
 def obfuscate(rest_response):
-    data_points_to_obfuscate=["pId1","userName","password","firstName","lastName","dateOfBirth","gender","salutation","title","nativeFirstName","nativeLastName","isoCountryOfBirth","dateOfBirth","cityOfBirth","email","street","zipCode","city","isoCountry"]
     rest_response=rest_response
-    if isinstance(rest_response,dict):
+
+    data_to_obfuscate=("userName","password","firstName","lastName","dateOfBirth","gender","salutation","title","nativeFirstName","nativeLastName","isoCountryOfBirth","dateOfBirth","cityOfBirth","email","emailConfirmed","street","zipCode","city","pId1","pId2","importKey","photoIdentificationType","identityNumber","idCardIssueDate","passportPhotoFileName","passportPhotoFileData","phone","fax","isoCountry","insuranceNumber","insuranceCompanyLocation","insuranceCompanyName","uasOperatorId","operatorType","reviewerName","managerName","managerName2","ssoUrl") #Define keys to be obfuscated
+    
+    if isinstance(rest_response,dict): #Some responses are not packed into list. In this instance, the dict is wrapped in a list
         rest_response=[rest_response]
-    if isinstance(rest_response,list):
+
+    if isinstance(rest_response,list): 
         json_response=rest_response
 
-        for eintrag in json_response:
-            def traversing_nested_lists(json_response):
-                nested_dict=json_response
-                print(nested_dict)
-                if isinstance(nested_dict,dict):
-                    for key,item in nested_dict.items():
-                        print(f"Iteriere über {key,item}")
-                        if isinstance(item,list):
-                            print(f"Rekursion bei: {key}")
-                            traversing_nested_lists(item[0])
-                        else:
-                            if key in data_points_to_obfuscate:
-                                item_encrypted=encryption(item)
-                                nested_dict[key]=item_encrypted
-                                print(f"Ersetze hier: {key,item}")
-        
-            traversing_nested_lists(eintrag)
+        for eintrag in json_response: #The function iterates over the response
+            traversing_nested_lists(eintrag,data_to_obfuscate) #This function iterates over all dictionary entries of the REST response
 
-    print(rest_response)
-    return rest_response
+    else:
+        return rest_response
 
-#rest_response=lplus_request(access_token)
-#obfuscated_response=obfuscate(rest_response)
-
-def decryption():
-    obfuscated_response=[{"test":b"o\xfe~i\xd2\xeb\x0f\xc3\xf0\xf7\xb6\xee_k4,\xe9\x83\xa9\xa0\x867\xd6\xee\xf4\x9d\xbd\x81\n\xd8}ln\xd7\xd3\xc1r\x9b.\xa4\xbe$\xac!D\xa4\xe8\xbc \x9a\x00\xffG2\x8b\xe01Xg\xe2|\x18\xdd\xa2\x10\x07jt\x94I\x00\xc7x\xa5w\xef\xf0\t\xb2\x84\xc7\xd6U\x9a\xc2A\n[\xc6r\xdau\x17\xae\x8bN4\x10\xc6\x81]\xc1&\xf8\x17\xe0>T\xe2\xfaxo\xe3\x9e\xf9\xa3S&\x19\x14\x92@0\x92\xf6)\xed\xbe\xef\x99\x9e\xe4\xe0\x90\xf8>D\x93O\xca\n\x90\x86v\x0c\xf1\xf3\xb6\xa5\xe9\xaf\xee\xa2\xb6\xc4\x14Fk\xa3Eb\x91<}[\xbd6\xd6\x93\xd5\x1c\x8a\x16s\x00\xce\xf4\x84$\x19\x9e\xecX\xcd\x94\xaa-\xe1\x97\xfe\x8c\x95\xdbJ\x11U\xf1\xd9<p\xc74\xb8\x1b\xa0\x8d\xdb\xf6\xcd\xf9\xba\xc1\xcd\xa8\x9eW\xc0\xdfU1:\xee,\xea%\x93\x00\x93Y\x8b\xbf\xc3\xc2\x9cc^\xd7\x1dph\xd0h\xa2\x96\x93$\xccE\xdbH\xca\xb1(}F*"}]
-    for key,item in obfuscated_response[0].items():
-        if isinstance(item,bytes):
-            with open("private_key.pem", "rb") as key_file:
-                private_key = serialization.load_pem_private_key(
-                    key_file.read(),
-                    password=None,
-                    backend=default_backend()
-        )
-            original_message = private_key.decrypt(
-            item,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
-            )
-            print(key, item, original_message,"\n")
+    return json_response
